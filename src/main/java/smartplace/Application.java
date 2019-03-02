56fdda8b123452111b5f88af0b686ceab890c333
omrishapira <omrishapira@192.168.0.120>
2019-03-02 23:10:19 +0200

    First commit with java classes

---
+++ src/main/java/smartplace/Application.java
@@ -0,0 +1,7 @@
+package smartplace;
+
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Hello World from Integrative!");
+	}
+}
---
+++ src/test/java/smartplace/ApplicationTest.java
@@ -0,0 +1,5 @@
+package smartplace;
+
+public class ApplicationTest {
+
+}
